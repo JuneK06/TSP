c6a2bdb12dfb0166843623aee2c415dc61fc9021
JuneK06 <ka0wa6ju1n9-en314@ezweb.ne.jp>
2022-02-23 11:27:34 +0900

    初期

---
+++ TSP/TSP.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#define N 5
+
+typedef struct {
+	int x;
+	int y;
+} Coord;
+
+int main(void)
+{
+	Coord toshi[N] = {
+		{80, 85},
+		{60, 50},
+		{50, 45},
+		{30, 40},
+		{20, 25},
+	};
+
+	return 0;
+}
