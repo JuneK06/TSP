ca59859fcbfabe461ca4a8d2e21a788465879aeb
JuneK06 <ka0wa6ju1n9-en314@ezweb.ne.jp>
2022-06-07 23:32:28 +0900

    大幅変更

--- TSP/TSP.c
+++ TSP/TSP.c
@@ -1,62 +1,82 @@
 ﻿#include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+
 #define N 5
 
-double dist[N][N] = { {0} };
-double best = 0;
-int comb[N];
-int cnt[N + 1] = { 0 };
-int index[N];
+double dist[N][N] = { {0} }; /*都市間の距離*/
+double best = 0;             /*最短経路の長さ*/
+int index[N];                /*最短経路を格納するための配列*/
 
 typedef struct {
 	int x;
 	int y;
-} Coord;
+} City;
 
-double calc_dist(Coord *a, Coord *b)
+double calc_dist(City* a, City* b)
 {
 	return sqrt(pow((double)(a->x) - (b->x), 2.0) + pow((double)(a->y) - (b->y), 2.0));
 }
 
-void swap(int i)
+void swap(int i, int j, int v[])
 {
-	int tmp = comb[0];
-	comb[0] = comb[i];
-	comb[i] = tmp;
+	int tmp = v[j];
+	v[j] = v[i];
+	v[i] = tmp;
 }
 
-void sum()
+/* 経路の距離を求める関数 */
+void sum(int v[])
 {
 	double tmp = 0;
 	int i;
 	for (i = 0; i < N; i++) {
-		tmp += dist[comb[i]][comb[(i + 1) % N]];
+		tmp += dist[v[i]][v[(i + 1) % N]];
 	}
 	if (best > tmp) {
 		best = tmp;
 		for (i = 0; i < N; i++) {
-			index[i] = comb[i];
+			index[i] = v[i];
 		}
 	}
 }
 
-void count(int n)
+/* 全ての経路の組み合わせを求める関数 */
+void route(int cnt, int v[])
 {
-	cnt[n]++;
-	if (cnt[n] > n) {
-		cnt[n] = 0;
-		count(n + 1);
-	}
-	else if (cnt[N] == 0) {
-		swap(n);
-		sum();
+	int i, j;
+	int tmp[N];
+	v[cnt] = cnt;
+
+	for (i = cnt; i > 0; i--) {
+		for (j = 0; j < N; j++) {
+			tmp[j] = v[j];
+			printf("%d", v[j]);
+		}
+		putchar('\n');
+
+		if (cnt < N - 1) {
+			route(cnt + 1, v);
+			for (j = 0; j < N; j++) {
+				v[j] = tmp[j];
+			}
+		}
+		else {
+			sum(v);
+		}
+
+		if (cnt < 3 || i == 1) {
+			break;
+		}
+		swap(i, i - 1, v);
 	}
 }
 
-int main(void)
+void main(void)
 {
 	int i, j;
-	Coord toshi[N] = {
+	int v[N] = { 0 };
+	City city[N] = {
 		{80, 85},
 		{60, 50},
 		{50, 45},
@@ -64,31 +84,31 @@ int main(void)
 		{20, 25},
 	};
 
+	/* 各都市の座標の表示 */
 	for (i = 0; i < N; i++) {
-		comb[i] = i;
-		printf("%d : %d, %d\n", i, toshi[i].x, toshi[i].y);
+		printf("%d : %d, %d\n", i, city[i].x, city[i].y);
 	}
 
+	/* 各都市間の距離を求める */
 	for (i = 0; i < N; i++)
 		for (j = i + 1; j < N; j++) {
-			dist[i][j] = calc_dist(&toshi[i], &toshi[j]);
+			dist[i][j] = calc_dist(&city[i], &city[j]);
 			dist[j][i] = dist[i][j];
 		}
 
+	/* bestに基準の距離を代入 */
 	for (i = 0; i < N; i++) {
 		best += dist[i][(i + 1) % N];
 	}
 
-	do {
-		count(1);
-	} while (cnt[N] == 0);
+	/* 最短経路を求める */
+	route(1, v);
 
+	/* 結果の表示 */
 	printf("最短距離：%f\n", best);
 	printf("経路：");
 	for (i = 0; i < N; i++) {
 		printf("%d→", index[i]);
 	}
 	printf("%d\n", index[0]);
-
-	return 0;
 }
